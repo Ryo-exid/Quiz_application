a02f6450ff0fb1f8155152ded2feed243ac9c780
Ryo-exid <sakura4oneok@gmail.com>
2021-04-29 21:10:57 +0900

    [Add]複数quiz/正誤判定

--- app.js
+++ app.js
@@ -1,23 +1,51 @@
-const question = 'ゲーム市場、最も売れたゲーム機は次のうちどれ？';
-const answers = [
-  'スーパーファミコン',
-  'プレイステーション2',
-  'ニンテンドースイッチ',
-  'ニンテンドーDS'
+const quiz = [
+  {
+    question: 'ゲーム市場、最も売れたゲーム機は次のうちどれ？',
+    answers: [
+      'スーパーファミコン',
+      'プレイステーション2',
+      'ニンテンドースイッチ',
+      'ニンテンドーDS'
+    ],
+    correct: 'ニンテンドーDS'
+  }, {
+    question: '日本語と同様の適切な語句を選びなさい　"夕暮れ"',
+    answers: [
+      'midday',
+      'twilight',
+      'daytime',
+      'midnight'
+    ],
+    correct: 'twilight'
+  }, {
+    question: '日本語と同様の適切な語句を選びなさい　"成功する"',
+    answers: [
+      'prosper',
+      'overtake',
+      'invest',
+      'finance'
+    ],
+    correct: 'prosper'
+  }
 ];
 
-const correct = 'ニンテンドーDS';
+const quizLength = quiz.length;
+let quizIndex = 0;
+
+
+
+
 
 const $button =document.getElementsByTagName('button');
 const buttonLength = $button.length;
 
 //クイズの問題文、選択肢を定義
 const setupQuiz = ()=> {
-  document.getElementById('js-question').textContent = question;
+  document.getElementById('js-question').textContent = quiz[quizIndex].question;
   let buttonIndex = 0;
   while(buttonIndex < buttonLength){
     //ここに命令
-    $button[buttonIndex].textContent = answers[buttonIndex];
+    $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
     buttonIndex++;
   }
 }
@@ -26,10 +54,20 @@ setupQuiz();
 
 
 const clickHandler = (e) => {
-  if(correct === e.target.textContent){
+  if(quiz[quizIndex].correct === e.target.textContent){
     window.alert('正解！')
   } else {
     window.alert('不正解！')
+  }
+
+  quizIndex++;
+
+  if(quizIndex < quizLength){
+    //問題数がまだあればこちらを実行
+    setupQuiz();
+  } else {
+    //問題数がもう無ければこちらを実行
+    window.alert('終了！')
   }
 };
 
@@ -49,7 +87,18 @@ while (handlerIndex < buttonLength) {
 
 
 
-// クイズの問題文、選択肢を定義　　※手順（1）
+// クイズの問題文、選択肢を定義　　※手順①
+
+// const question = 'ゲーム市場、最も売れたゲーム機は次のうちどれ？';
+// const answers = [
+//   'スーパーファミコン',
+//   'プレイステーション2',
+//   'ニンテンドースイッチ',
+//   'ニンテンドーDS'
+// ];
+
+// const correct = 'ニンテンドーDS';
+
 
 
 //定数の文字列をHTMLに反映させる
@@ -63,6 +112,9 @@ while (handlerIndex < buttonLength) {
 // document.getElementsByTagName('button')[2].textContent = answers[2];
 // document.getElementsByTagName('button')[3].textContent = answers[3];
 
+
+
+//※手順② （手順①をリファクタリング）
 
 //HTMLのオブジェクトを取ってくる場合、「$」を入れる
 // const $button =document.getElementsByTagName('button');
@@ -82,7 +134,7 @@ while (handlerIndex < buttonLength) {
 
 
 
-//ボタンをクリックしたら正誤判定 ※手順（2）
+//ボタンをクリックしたら正誤判定 ※手順③（手順②をリファクタリング）
 
 //文字列を比較して一致するかどうかをみる場合は「===」を使う
 
@@ -121,7 +173,7 @@ while (handlerIndex < buttonLength) {
 
 
 
-//※手順（3）　（手順（2）をリファクタリング）
+//※手順④　（手順③をリファクタリング）
 
 // $button[0].addEventListener('click', (e)=> {
 //   clickHandler(e);
